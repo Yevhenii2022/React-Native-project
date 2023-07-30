fa2964bca5ee016978efd480cb9d3c41979efb00
Yevhenii2022 <3starosta3@gmail.com>
2023-07-30 18:29:35 +0300

    add firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,38 @@
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { getAuth, initializeAuth } from 'firebase/auth';
+import { getReactNativePersistence } from 'firebase/auth/react-native';
+import { getFirestore } from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+	API_KEY,
+	AUTH_DOMAIN,
+	PROJECT_ID,
+	STORAGE_BUCKET,
+	MESSAGING_SENDER_ID,
+	APP_ID,
+	MEASUREMENT_ID,
+} from '@env';
+
+const firebaseConfig = {
+	apiKey: API_KEY,
+	authDomain: AUTH_DOMAIN,
+	projectId: PROJECT_ID,
+	storageBucket: STORAGE_BUCKET,
+	messagingSenderId: MESSAGING_SENDER_ID,
+	appId: APP_ID,
+	measurementId: MEASUREMENT_ID,
+};
+
+let app;
+let auth;
+if (getApps().length < 1) {
+	app = initializeApp(firebaseConfig);
+	auth = initializeAuth(app, {
+		persistence: getReactNativePersistence(AsyncStorage),
+	});
+} else {
+	app = getApp();
+	auth = getAuth();
+}
+export { app, auth };
+export const db = getFirestore(app);
