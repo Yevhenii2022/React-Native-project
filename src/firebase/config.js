0b2ebb8e9c40b9ce61faf2c3206c947fb3650b1f
Yevhenii2022 <3starosta3@gmail.com>
2023-08-09 07:46:39 +0300

    add

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -12,6 +12,15 @@ import {
 	MEASUREMENT_ID,
 } from '@env';
 
+// API_KEY = 'AIzaSyAmqIKCzTNuocftlZLlsMKN7ntNUvF8n58';
+// AUTH_DOMAIN = 'react-native-project-20ba6.firebaseapp.com';
+// PROJECT_ID = 'react-native-project-20ba6';
+// STORAGE_BUCKET = 'react-native-project-20ba6.appspot.com';
+// MESSAGING_SENDER_ID = '790050927949';
+// APP_ID = '1:790050927949:web:9da5392215ff1b5c5cf304';
+// MEASUREMENT_ID = 'G-HCPX2L1LZH';
+// react-native-project-20ba6
+
 const firebaseConfig = {
 	apiKey: API_KEY,
 	authDomain: AUTH_DOMAIN,
